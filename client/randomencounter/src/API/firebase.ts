db6fa9dc203b6cb9bedd26fd080a64506d79cef0
Adam D'Andrea <Adam.DAndrea@atco.com>
2022-02-18 13:17:42 -0800

    initiate firebase

---
+++ client/randomencounter/src/API/firebase.ts
@@ -0,0 +1,34 @@
+import { initializeApp } from "firebase/app";
+import { getFunctions } from "firebase/functions";
+import {
+  ApplicationVerifier,
+  Auth,
+  ConfirmationResult,
+  getAuth,
+  signInAnonymously,
+} from "firebase/auth";
+import { getDatabase } from "firebase/database";
+// import firebase from '../node_modules/firebase/index/firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBpXoL8NGZInZniUC1Xl6qofJnsJkYBJd8",
+  authDomain: "randomencounter-54d6d.firebaseapp.com",
+  projectId: "randomencounter-54d6d",
+  storageBucket: "randomencounter-54d6d.appspot.com",
+  messagingSenderId: "797619872921",
+  appId: "1:797619872921:web:609ddfed8e56de5bacc408",
+  measurementId: "G-BB5LNHNCPJ",
+};
+
+const app = initializeApp(firebaseConfig);
+export const authenticate = getAuth(app);
+
+export const dataBase = getDatabase();
+
+export const functions = getFunctions(app);
+// export const anonSignIn = async () => signInAnonymously(auth);
+export declare function signInWithPhoneNumber(
+  auth: Auth,
+  phoneNumber: string,
+  appVerifier: ApplicationVerifier
+): Promise<ConfirmationResult>;
