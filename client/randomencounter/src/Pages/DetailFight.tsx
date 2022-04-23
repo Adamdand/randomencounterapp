c729aa291d48a2441ebb7a4580b55d0de6ca9e9c
Adam D'Andrea <Adam.DAndrea@atco.com>
2022-04-22 18:59:52 -0600

    add monsters to detailed fight

--- client/randomencounter/src/Pages/DetailFight.tsx
+++ client/randomencounter/src/Pages/DetailFight.tsx
@@ -3,10 +3,17 @@ import { makeStyles } from "@mui/styles";
 import React, { useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { CustomerDataContext } from "../Context/CustomerContext";
-import { IPlayer } from "../Context/Types";
+import {
+  IMonster,
+  IMonsterDetails,
+  IPlayer,
+  IRandomMonster,
+} from "../Context/Types";
 import CharacterCard from "./CharacterCard";
 import { defaultUserData } from "../API/DummyData";
 import CreateNewPlayer from "./CreateNewPlayer";
+import monsterAPIs from "../API/monsterAPI";
+import { defaultMonsterDetails } from "../Context/DefaultTypes";
 
 interface IProps {
   gameType: string;
@@ -184,14 +191,197 @@ const DetailedFight = (props: IProps) => {
     }
   };
 
+  const [monsterList, setMonsterList] = useState<IMonster[]>([]);
+  const [monsterRatingList, setMonsterRatingList] = useState<IMonster[]>([]);
+
+  const [monsterDetails, setMonsterDetails] = useState<IMonsterDetails>(
+    defaultMonsterDetails
+  );
+  const [averagePlayerLevel, setAveragePlayerLevel] = useState<number>(3);
+  const [randomActivityNumber, setRandomActivityNumber] = useState<number>(0);
+  const [randomMonsterTypeOne, setRandomMonsterTypeOne] =
+    useState<IRandomMonster>({
+      quantity: 0,
+      index: "null",
+      name: "null",
+      url: "null",
+    });
+  const [randomMonsterTypeTwo, setRandomMonsterTypeTwo] =
+    useState<IRandomMonster>({
+      quantity: 0,
+      index: "null",
+      name: "null",
+      url: "null",
+    });
+  const [twoTypeOfMonsters, settwoTypeOfMonsters] = useState<boolean>(false);
+  //   const [selectedMonster, setSelectedMonster] = useState<IMonster>({
+  //     index: "null",
+  //     name: "null",
+  //     url: "null",
+  //   });
+  // const { state } = useContext(IdContext);
+  console.log("monsters on main page", monsterList);
+
+  const changeAveragePlayerLevel = (playerLevel: string) => {
+    setAveragePlayerLevel(Number(playerLevel));
+  };
+
+  const monsterActivities = [
+    "playing chess",
+    "gossiping about Brenda",
+    "planning an attack on a nearby town",
+    "playing Dungeons and Dragons",
+    "roasting a pig",
+    "sleeping",
+    "cleaning their teeth after having just eaten",
+    "wondering around looking lost",
+    "trying on tiny hats",
+    "clipping their toe nails",
+    "eating a child alive",
+    "mumbling to themself",
+    "doing just general nasty stuff",
+    "planning a birthday party for their grandmother",
+  ];
+
+  const setNewMonsterActivity = () =>
+    setRandomActivityNumber(
+      Math.floor(Math.random() * monsterActivities.length - 1) + 1
+    );
+
+  let tempList: IMonster[];
+  const getAllMonsters = async () => {
+    try {
+      tempList = await monsterAPIs.getAllMonsterAxios();
+    } catch (error) {
+      console.log("error: ", error);
+    }
+    if (tempList.length > 0) {
+      setMonsterList(tempList);
+    } else {
+      setMonsterList([]);
+    }
+  };
+
+  useEffect(() => {
+    getAllMonsters();
+  }, []);
+
+  const getMonsterDetails = async (monsterName: string) => {
+    let tempMonsterDetails: IMonsterDetails;
+    if (monsterName !== null) {
+      try {
+        tempMonsterDetails = await monsterAPIs.getSpecificMonsterAxios(
+          monsterName
+        );
+        setMonsterDetails(tempMonsterDetails);
+        console.log("monster details :", tempMonsterDetails);
+      } catch (error) {
+        console.log("error: ", error);
+      }
+    }
+  };
+
+  const getRandomMonsterTypeOne = async (monsters: IMonster[]) => {
+    console.log("FUNCTION MONSTERS WITH RATING LIST", monsters);
+    const monsterIndex = Math.floor(Math.random() * monsters.length);
+    let rndInt: number;
+    if (twoTypeOfMonsters === true) {
+      rndInt = Math.floor(Math.random() * 3) + 1;
+    } else {
+      rndInt = Math.floor(Math.random() * 6) + 1;
+    }
+    const monsterObject = monsters[monsterIndex];
+    console.log("monsterObject", monsterObject);
+
+    const newRandomMonster = {
+      ...monsterObject,
+      quantity: rndInt,
+    } as IRandomMonster;
+    try {
+      const tempMonsterDetails = monsterAPIs.getSpecificMonsterAxios(
+        newRandomMonster.index
+      );
+
+      testList.push({
+        characterName: (await tempMonsterDetails).name,
+        characterAC: (await tempMonsterDetails).armor_class,
+        characterHealth: (await tempMonsterDetails).hit_points,
+        characterInitative: Math.ceil(
+          Math.round(((await tempMonsterDetails).dexterity - 10) / 2) +
+            (Math.random() * (20 - 1) + 1)
+        ),
+        characterLevel: (await tempMonsterDetails).challenge_rating,
+        characterMaxHealth: (await tempMonsterDetails).hit_points,
+        type: "npc",
+      });
+      setRandomMonsterTypeOne(newRandomMonster);
+    } catch (error) {
+      console.log(error);
+    }
+    console.log("1 random monster: ", randomMonsterTypeOne);
+  };
+
+  const getRandomMonsterTypeTwo = async (monsters: IMonster[]) => {
+    if (twoTypeOfMonsters === true && monsters.length > 1) {
+      const monsterIndex = Math.floor(Math.random() * monsters.length);
+      const rndInt = Math.floor(Math.random() * 3) + 1;
+      const monsterObject = monsters[monsterIndex];
+      const newRandomMonster = {
+        ...monsterObject,
+        quantity: rndInt,
+      } as IRandomMonster;
+      try {
+        const tempMonsterDetails = monsterAPIs.getSpecificMonsterAxios(
+          newRandomMonster.index
+        );
+
+        testList.push({
+          characterName: (await tempMonsterDetails).name,
+          characterAC: (await tempMonsterDetails).armor_class,
+          characterHealth: (await tempMonsterDetails).hit_points,
+          characterInitative: Math.ceil(
+            Math.round(((await tempMonsterDetails).dexterity - 10) / 2) +
+              (Math.random() * (20 - 1) + 1)
+          ),
+          characterLevel: (await tempMonsterDetails).challenge_rating,
+          characterMaxHealth: (await tempMonsterDetails).hit_points,
+          type: "npc",
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const findRandomEncounter = (monsters: IMonster[]) => {
+    settwoTypeOfMonsters(Math.random() < 0.5);
+
+    getRandomMonsterTypeOne(monsters);
+    getRandomMonsterTypeTwo(monsters);
+  };
+
+  const getMonsterWithRatingBtn = async () => {
+    if (averagePlayerLevel !== undefined) {
+      try {
+        const monstersWithCR = await monsterAPIs.getMonstersWithRating(
+          averagePlayerLevel
+        );
+        setMonsterRatingList(monstersWithCR);
+        if (monstersWithCR.length > 0) {
+          findRandomEncounter(monstersWithCR);
+          setNewMonsterActivity();
+        }
+      } catch (error) {
+        console.log("error: ", error);
+      }
+    }
+  };
+
   return (
     <Box
       className={classes.root}
       sx={{ display: "flex", flexDirection: "row" }}
     >
-      {/* <Box sx={{ justifyContent: "center" }}>
-        <CharacterInput inputList={inputList} setInputList={setInputList} />
-      </Box> */}
       <Box sx={{ width: "50%" }}>
         <Box>Characters in Fight:</Box>
 
@@ -263,6 +453,45 @@ const DetailedFight = (props: IProps) => {
           </Button>
         </Box>
       </Box>
+      <Button onClick={getMonsterWithRatingBtn}>
+        <Typography>Get Monsters</Typography>
+      </Button>
+      {monsterRatingList.length > 0 && randomMonsterTypeOne !== undefined && (
+        <Box>
+          <Box>
+            <Typography
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+              style={{ cursor: "pointer" }}
+            >
+              you have encountered {randomMonsterTypeOne.quantity}
+              <Box>
+                <Button
+                  onClick={() => {
+                    getMonsterDetails(randomMonsterTypeOne.index);
+                  }}
+                >
+                  {randomMonsterTypeOne?.name}
+                </Button>
+              </Box>
+              {twoTypeOfMonsters && randomMonsterTypeTwo.quantity !== 0 && (
+                <Typography>
+                  and {randomMonsterTypeTwo.quantity}
+                  <Button
+                    onClick={() => {
+                      getMonsterDetails(randomMonsterTypeTwo.index);
+                    }}
+                  >
+                    {randomMonsterTypeTwo.name}
+                  </Button>
+                </Typography>
+              )}
+              {monsterActivities[randomActivityNumber]}.
+            </Typography>
+          </Box>
+        </Box>
+      )}
     </Box>
   );
 };
